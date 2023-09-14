1284071b81193a96a17b0c4b0016f8701fe451e6
lalubramadhan <lalubagoes2@gmail.com>
2023-09-14 22:59:48 +0700

    first commit

---
+++ app/src/main/java/com/code/datastore/ViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.code.datastore
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory(private val pref: SettingPreference) : ViewModelProvider.NewInstanceFactory(){
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(MainViewModel::class.java)){
+            return MainViewModel(pref) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
+}
